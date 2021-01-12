fea0c4bbc936e70b233d9d52fddb1962d99ba322
kahirokunn <okinakahiro@gmail.com>
2021-01-12 11:29:51 +0900

    installed jsonpath for simplify the code.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as yaml from 'js-yaml';
+import jp from 'jsonpath';
 
 function snakecase(str: string) {
   let result = '';
@@ -15,107 +16,24 @@ function snakecase(str: string) {
   return result;
 }
 
-function propertiesToSnakeCase(properties: Record<string, any>) {
-  const result: Record<string, any> = {};
-  Object.entries(properties).forEach(([propertyName, value]) => {
-    result[snakecase(propertyName)] = value;
-  });
-  return result;
-}
-
-function requiredToSnakeCase(required: string[]) {
-  return required.map((propertyName) => snakecase(propertyName));
-}
-
-function isObject(v: any): v is Record<string, any> {
-  return typeof v === 'object' && v !== null;
-}
-
-function camelCasePropertiesToSnakeCase(obj: any) {
-  if (isObject(obj)) {
-    const schemas: Record<string, any> = {};
-    Object.entries(obj).forEach(([schemaName, schema]: [string, any]) => {
-      schemas[schemaName] = {
-        ...schema,
-      };
-
-      if (schemas[schemaName].properties) {
-        schemas[schemaName].properties = camelCasePropertiesToSnakeCase(
-          propertiesToSnakeCase(schemas[schemaName].properties),
-        );
-      }
-
-      if (
-        isObject(schemas[schemaName].additionalProperties) &&
-        isObject(schemas[schemaName].additionalProperties.properties)
-      ) {
-        schemas[
-          schemaName
-        ].additionalProperties.properties = camelCasePropertiesToSnakeCase(
-          propertiesToSnakeCase(
-            schemas[schemaName].additionalProperties.properties,
-          ),
-        );
-      }
-
-      if (schemas[schemaName].required) {
-        schemas[schemaName].required = requiredToSnakeCase(
-          schemas[schemaName].required,
-        );
-      }
-    });
-
-    return schemas;
-  }
-  return obj;
+function renameKeys<
+  T extends Record<string, any>,
+  U extends Record<string, any> = T
+>(renameFn: (s: string) => string) {
+  return (v: T) =>
+    Object.keys(v).reduce(
+      (acc, k) => ({ ...acc, [renameFn(k)]: v[k] }),
+      {} as U,
+    );
 }
 
 export function swaggerCamelCasePropertiesToSnakeCase(swaggerYaml: string) {
-  let swagger = yaml.load(swaggerYaml) as any;
+  const swagger = yaml.load(swaggerYaml);
 
-  if (swagger && swagger.components && swagger.components.schemas) {
-    swagger = {
-      ...swagger,
-      components: {
-        ...swagger.components,
-        schemas: camelCasePropertiesToSnakeCase(swagger.components.schemas),
-      },
-    };
-  }
-  if (swagger.paths) {
-    Object.values(swagger.paths).forEach((path: any) => {
-      Object.values(path).forEach((pathSchema: any) => {
-        if (pathSchema.requestBody) {
-          Object.keys(pathSchema.requestBody.content).forEach((contentType) => {
-            pathSchema.requestBody.content[
-              contentType
-            ] = camelCasePropertiesToSnakeCase(
-              pathSchema.requestBody.content[contentType],
-            );
-          });
-        }
-        if (pathSchema.parameters) {
-          pathSchema.parameters.forEach((param: any) => {
-            param.name = snakecase(param.name);
-          });
-        }
-        if (pathSchema.responses) {
-          Object.values(pathSchema.responses).forEach((httpSchema: any) => {
-            if (httpSchema.content) {
-              Object.keys(httpSchema.content).forEach((contentType) => {
-                if (httpSchema.content[contentType].schema.properties) {
-                  httpSchema.content[
-                    contentType
-                  ] = camelCasePropertiesToSnakeCase(
-                    httpSchema.content[contentType],
-                  );
-                }
-              });
-            }
-          });
-        }
-      });
-    });
-  }
+  // best is: `jp.apply(swagger, '$..properties.*~', snakecase)`
+  // but package does not support ~ query.
+  jp.apply(swagger, '$..properties', renameKeys(snakecase));
+  jp.apply(swagger, '$..parameters[*].name', snakecase);
+  jp.apply(swagger, '$..required[*]', snakecase);
   return yaml.dump(swagger);
 }
